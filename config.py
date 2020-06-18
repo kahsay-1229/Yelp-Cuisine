0290f46313c72c136279dffed2e7c1ffd0c23011
kahsay <kahsaywake@gmail.com>
2020-06-18 19:55:27 -0400

    first commit

---
+++ config.py
@@ -0,0 +1,5 @@
+ykey = 'tNVSPxTZuK97fliYWToZrZpCGASCD0HlyF7s554dF_IdGdO1vAenbda05dWc6zUrsvXPE25NpvVJw1ZhohHYp-4n68rsIzrDPQAwh0lceNXGdjprDYMTAvHM_oMvXnYx' 
+gkey = 'AIzaSyCu1ZA_-wimdbPaUssBz2Y3IjJQ_3FEaGU'
+api_url = 'https://api.yelp.com'
+search_path = '/v3/businesses/search'
+business_path = '/v3/businesses/'
